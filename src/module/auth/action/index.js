7f3d9d9b4ffc774ba11427de1cc23826493024cf
CongThanh <dinhcongthanh1910@gmail.com>
2021-02-23 16:53:13 +0700

    api login

--- src/module/auth/action/index.js
+++ src/module/auth/action/index.js
@@ -3,7 +3,13 @@ import toastr from "toastr";
 import fetchHelper from "../../../helpers/FetchHelper";
 
 const mapDispatchToAC = {
-  loginSuccess: (result, email) => ({ result, email }),
+  loginSuccess: (result) => ({ result }),
+  login: (data) => async ({ Api }) => {
+    let { result, status } = await Api.post(`user/login`, data);
+    if (status === 200) {
+      console.log("result: ", result);
+    }
+  },
   logoutSuccess: () => ({}),
   logout: (isTokenExpired = false) => async ({ Api }) => {
     !isTokenExpired && authDispatcher.deleteNotifyToken({ deviceId: "" });
