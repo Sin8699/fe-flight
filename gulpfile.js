3f21865ad66aa9712a6804f383a7cbd47f75aab8
Phuong <phuong.dao@inkr.com>
2021-10-19 21:23:02 +0700

    to deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,47 @@
-const gulp = require('gulp');
-const requireDir = require('require-dir');
+const gulp = require("gulp");
+const requireDir = require("require-dir");
 
-requireDir('./gulp');
+requireDir("./gulp");
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 |  Compile
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('compile', gulp.parallel('style', 'script', 'script:webpack', 'vendor'));
-gulp.task('compile:all', gulp.parallel('compile', 'pug'));
+gulp.task(
+  "compile",
+  gulp.parallel("style", "script", "script:webpack", "vendor")
+);
+gulp.task("compile:all", gulp.parallel("compile", "pug"));
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 |  Deploy
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('build', gulp.series('clean:build', 'build:static', 'compile:all'));
-gulp.task('build:test', gulp.series('build', 'watch'));
-gulp.task('live', gulp.series('clean:live', 'build', 'build:push'));
+gulp.task("build", gulp.series("clean:build", "build:static", "compile:all"));
+gulp.task("build:test", gulp.series("build", "watch"));
+gulp.task("live", gulp.series("clean:live", "build", "build:push"));
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 |  Run development environment
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('default', gulp.series('clean', 'compile', 'watch'));
+gulp.task("default", gulp.series("clean", "compile", "watch"));
+
+gulp.task("serve", function () {
+  browserSync({
+    server: {
+      baseDir: "./",
+    },
+    port: process.env.PORT || 5000,
+  });
+
+  gulp.watch(
+    [
+      "*.html",
+      "css/*.css",
+      "js/*.js",
+      "views/*.html",
+      "template/*.html",
+      "./*.html",
+    ],
+    { cwd: "app" },
+    reload
+  );
+});
